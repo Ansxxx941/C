f9f30fe648541e312ed1cf4e372f47d7845bfad6
Ansxxx941 <yuxyanas@gmail.com>
2024-07-19 08:57:21 -0400

    const variable

---
+++ const.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+ int main()
+
+ {
+ //.const variable cannot be changed //.
+   const  int NUMBER= 12;
+
+           printf("%d",NUMBER);
+
+             return 0;
+
+
+
+
+}
