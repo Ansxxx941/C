31a3b5ad89f19e98d128dac5e951f4b980552e52
Ansxxx941 <yuxyanas@gmail.com>
2024-07-19 17:11:23 -0400

    simple basic calcluator

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+   
+    int main ()
+{
+  float numb1=2.5;
+  float  numb2=2.5;
+
+ printf("Enter the number : ");
+   scanf("%f",&numb1);
+ printf("Enter the second  number : ");
+ scanf("%f",&numb2);
+
+ printf("the number is %f+%f=%f",numb1,numb2,numb1+numb2);
+
+       return 0;
+
+
+}
