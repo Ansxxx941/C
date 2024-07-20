8a1dc5c7c41238207788488997a464b60a75648c
Ansxxx941 <yuxyanas@gmail.com>
2024-07-19 20:00:05 -0400

    if

---
+++ if.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void  Numb(int a,int b){
+
+if (a > b){
+
+printf("the number %d is higher than the number %d",a,b);
+}
+
+else{
+
+printf("the number %d is higher than the number %d",b,a);
+
+}
+
+
+}
+
+
+int main(){
+
+ Numb(5,8);
+
+
+
+
+   return 0;
+
+}
